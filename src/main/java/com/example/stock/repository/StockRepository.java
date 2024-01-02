3d049585f00086be5f3f3e5d3d15d09eb7fca3c0
banggeunho <panggeunho@gmail.com>
2024-01-02 23:21:41 +0900

    Feat: Service 생성

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -1,7 +1,10 @@
 package com.example.stock.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.stock.domain.Stock;
 
+@Repository
 public interface StockRepository extends JpaRepository<Stock, Long>{
 }
